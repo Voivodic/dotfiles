a39d3c6c78deccb8535a057f90c109ecc12c024d
Voivodic <rodrigo.voivodic@gmail.com>
2024-11-24 06:51:37 +0100

    Installation of ncurses and zsh

--- install_initial_configurations.sh
+++ install_initial_configurations.sh
@@ -13,3 +13,43 @@ then
     echo "wget is not installed. Please, install it to procced."
     exit 1
 fi
+
+# Check if tar is installed
+if ! command -v tar &> /dev/null
+then
+    echo "tar is not installed. Please, install it to procced."
+    exit 1
+fi
+
+# Download and extract ncurses source code
+wget https://ftp.gnu.org/gnu/ncurses/ncurses-6.3.tar.gz
+tar -xzf ncurses-6.3.tar.gz
+cd ncurses-6.3
+
+# Configure, compile, and install ncurses
+./configure --prefix=$HOME/.local --with-shared --without-debug --enable-widec
+make
+make install
+cd ..
+
+# Update environment variables
+echo 'export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+echo 'export CFLAGS="-I$HOME/.local/include"' >> ~/.bashrc
+echo 'export LDFLAGS="-L$HOME/.local/lib"' >> ~/.bashrc
+source ~/.bashrc
+
+# Download and extract Zsh source code
+wget https://sourceforge.net/projects/zsh/files/latest/download -O zsh.tar.xz
+tar -xf zsh.tar.xz
+cd zsh-*
+
+# Configure, compile, and install Zsh
+./configure --prefix=$HOME/.local
+make
+make install
+
+# Add Zsh to PATH and set it as the default shell
+echo 'exec $HOME/.local/bin/zsh -l' >> ~/.bashrc
+source ~/.bashrc
+
