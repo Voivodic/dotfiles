57d117367aa1f52aefef147b4cb6c571864597ab
Voivodic <rodrigo.voivodic@gmail.com>
2024-11-24 07:21:12 +0100

    Extra if/else

--- install_initial_configurations.sh
+++ install_initial_configurations.sh
@@ -36,37 +36,42 @@ then
 fi
 
 {
-    # Download and extract ncurses source code
-    wget https://ftp.gnu.org/gnu/ncurses/ncurses-6.3.tar.gz
-    tar -xzf ncurses-6.3.tar.gz
-    cd ncurses-6.3
+    {
+        # Download and extract ncurses source code
+        wget https://ftp.gnu.org/gnu/ncurses/ncurses-6.3.tar.gz
+        tar -xzf ncurses-6.3.tar.gz
+        cd ncurses-6.3
 
-    # Configure, compile, and install ncurses
-    ./configure --prefix=$HOME/.local --with-shared --without-debug --enable-widec
-    make
-    make install
-    cd ..
-
-    # Update environment variables
-    echo 'export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-    echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
-    echo 'export CFLAGS="-I$HOME/.local/include"' >> ~/.bashrc
-    echo 'export LDFLAGS="-L$HOME/.local/lib"' >> ~/.bashrc
-    source ~/.bashrc
+        # Configure, compile, and install ncurses
+        ./configure --prefix=$HOME/.local --with-shared --without-debug --enable-widec
+        make
+        make install
+        cd ..
+    } && {
+        # Update environment variables
+        echo 'export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+        echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+        echo 'export CFLAGS="-I$HOME/.local/include"' >> ~/.bashrc
+        echo 'export LDFLAGS="-L$HOME/.local/lib"' >> ~/.bashrc
+        source ~/.bashrc
+    }
 } && echo -e "\n\n\n ncurses installed! \n\n\n"
 
 {
-    # Download and extract Zsh source code
-    wget https://sourceforge.net/projects/zsh/files/latest/download -O zsh.tar.xz
-    tar -xf zsh.tar.xz
-    cd zsh-*
+    {
+        # Download and extract Zsh source code
+        wget https://sourceforge.net/projects/zsh/files/latest/download -O zsh.tar.xz
+        tar -xf zsh.tar.xz
+        cd zsh-*
 
-    # Configure, compile, and install Zsh
-    ./configure --prefix=$HOME/.local
-    make
-    make install
-
-    # Add Zsh to PATH and set it as the default shell
-    echo 'exec $HOME/.local/bin/zsh -l' >> ~/.bashrc
-    source ~/.bashrc
+        # Configure, compile, and install Zsh
+        ./configure --prefix=$HOME/.local
+        make
+        make install
+        cd ..
+    } && {
+        # Add Zsh to PATH and set it as the default shell
+        echo 'exec $HOME/.local/bin/zsh -l' >> ~/.bashrc
+        source ~/.bashrc
+    }
 } && echo -e "\n\n\n zsh installed! \n\n\n"
