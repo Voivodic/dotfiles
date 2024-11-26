a41a0946188b0dc3d57b8e62f71cbae6e53ecfcf
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2024-11-26 00:26:16 +0000

    change the oh my zsh to starship and keep the bash terminal

--- install_initial_configurations.sh
+++ install_initial_configurations.sh
@@ -28,13 +28,6 @@ then
     exit 1
 fi
 
-# Check if make is installed
-if ! command -v make &> /dev/null
-then
-    echo "make is not installed. Please, install it to procced."
-    exit 1
-fi
-
 # Check if gcc is installed
 if ! command -v gcc &> /dev/null
 then
@@ -42,144 +35,57 @@ then
     exit 1
 fi
 
-# ncurses
+# Download and set (Hack) nerdfonts
 {
-    {
-        # Download and extract ncurses source code
-        wget https://ftp.gnu.org/gnu/ncurses/ncurses-6.3.tar.gz
-        tar -xzf ncurses-6.3.tar.gz
-        cd ncurses-6.3
-
-        # Configure, compile, and install ncurses
-        ./configure --prefix=$HOME/.local --with-shared --without-debug --enable-widec
-        make
-        make install
-        cd ..
-    } && {
-        # Update environment variables
-        export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH
-        export PATH=$HOME/.local/bin:$PATH
-        export CFLAGS=-I$HOME/.local/include
-        export LDFLAGS=-L$HOME/.local/lib
-
-        # Update .bashrc
-        echo 'export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-        echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
-        echo 'export CFLAGS="-I$HOME/.local/include"' >> ~/.bashrc
-        echo 'export LDFLAGS="-L$HOME/.local/lib"' >> ~/.bashrc
-    }
-} && {
-    rm -r ncurses*
-    echo -e "\n\n\n ncurses installed! \n\n\n" 
-} || {
-    echo "An error ocurred while trying to install ncurses!"
-    exit 1
-}
-
-# zsh
-{
-    {
-        # Download and extract Zsh source code
-        wget https://sourceforge.net/projects/zsh/files/latest/download -O zsh.tar.xz
-        tar -xf zsh.tar.xz
-        cd zsh-*
-
-        # Configure, compile, and install Zsh
-        ./configure --prefix=$HOME/.local
-        make
-        make install
-        cd ..
-    } && {
-        # Add Zsh to PATH and set it as the default shell
-        echo 'exec $HOME/.local/bin/zsh -l' >> ~/.bashrc
-    }
-} && {
-    rm -r zsh*
-    echo -e "\n\n\n zsh installed! \n\n\n" 
-} || {  
-    echo "An error ocurred while trying to install zsh!"
-    exit 1
-}
-
-# Install the nerd fonts
-{
-    mkdir -p ~/.local/share/fonts
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Hack.zip
     unzip Hack.zip
-    mv HackNerdFont-Regular.ttf ~/.local/share/fonts/
-} && {
-    rm -r Hack*
-    echo -e "\n\n\n nerd fonts installed! \n\n\n" 
-} || {  
-    echo "An error ocurred while trying to install nerd fonts!"
+    mkdir -p ~/.local/share/fonts/
+    mv *.ttf ~/.local/share/fonts/
+    fc-cache -f -v
+    rm Hack.zip
+} && echo -e "\n\n\n Nerdfonts installed! \n\n\n" || {
+    echo "An error ocurred while trying to install nerdfonts!"
     exit 1
 }
 
-# Install oh my zsh
+
+# Download and set starship for the terminal configuration
 {
-    rm -rf ~/.oh-my-zsh
-    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-} && echo -e "\n\n\n oh my zsh installed! \n\n\n" || {
-    echo "An error ocurred while trying to install oh my zsh!"
+    wget -q https://starship.rs/install.sh | sh
+    if ! grep -qF 'eval "$(starship init bash)"' ~/.bashrc; then
+        echo 'eval "$(starship init bash)"' >> ~/.bashrc
+    fi
+    rm install.sh
+} && echo -e "\n\n\n Starship installed! \n\n\n" || {
+    echo "An error ocurred while trying to install starship!"
     exit 1
 }
 
-# Install powerlevel10k
-{
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    sed -i 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
-} && echo -e "\n\n\n powerlevel10k installed! \n\n\n" || {
-    echo "An error ocurred while trying to install powerlevel10k!"
-    exit 1
-}
-
-# Install zsh syntax highlighting
-{
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-} && echo -e "\n\n\n zsh syntax highlighting installed! \n\n\n" || {
-    echo "An error ocurred while trying to install zsh syntax highlighting!"
-    exit 1
-}
-
-# Install zsh auto-suggestion
-{
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-} && echo -e "\n\n\n zsh autosuggestions installed! \n\n\n" || {
-    echo "An error ocurred while trying to install zsh auto-suggestion!"
-    exit 1
-}
-
-# Update the plugins on zshrc
-sed -i 's|plugins=(git)|plugins=(git zsh-syntax-highlighting  zsh-autosuggestions)|' ~/.zshrc && echo -e "\n\n\n plugings updatted! \n\n\n" || {
-    echo "An error ocurred while trying to update the plugins on .zshrc!"
-    exit 1
-}
-
-# Start zsh and configure it
-~/.local/bin/zsh
 
 # Install neovim
-{
-    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-    tar -xzf nvim-linux64.tar.gz
-    mv nvim-linux64 ~/.local/nvim
-    rm nvim-linux64.tar.gz
-    echo 'export PATH=$HOME/.local/nvim/bin:$PATH' >> ~/.zshrc
-    source ~/.zshrc
-} && echo -e "\n\n\n Neovim installed! \n\n\n" || {
-    echo "An error ocurred while trying to install neovim!"
-    exit 1
-}
+#{
+#    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+#    tar -xzf nvim-linux64.tar.gz
+#    mv nvim-linux64 ~/.local/nvim
+#    rm nvim-linux64.tar.gz
+#    if ! grep -qF 'export PATH=$HOME/.local/nvim/bin:$PATH' ~/.bashrc; then
+#        echo 'export PATH=$HOME/.local/nvim/bin:$PATH' >> ~/.bashrc
+#    fi
+#    source ~/.bashrc
+#} && echo -e "\n\n\n Neovim installed! \n\n\n" || {
+#    echo "An error ocurred while trying to install neovim!"
+#    exit 1
+#}
 
 # Configure Neovim
-{
-    tar -zxvf nvim_config.tar.gz -C ~/
-} && {
-    echo -e "\n\n\n Neovim configured! \n\n\n" 
-    nvim
-    } || {
-    echo "An error ocurred while trying to configure Neovim!"
-    exit 1
-}
+#{
+#    tar -zxvf nvim_config.tar.gz -C ~/
+#} && {
+#    echo -e "\n\n\n Neovim configured! \n\n\n" 
+#    nvim
+#    } || {
+#    echo "An error ocurred while trying to configure Neovim!"
+#    exit 1
+#}
 
 echo -e "\n\n\n ALL DONE! \n\n\n"
