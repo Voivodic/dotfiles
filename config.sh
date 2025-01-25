8118a2650c22a10cad6a2c7f71917a55e5c69909
Voivodic <rodrigo.voivodic@gmail.com>
2025-01-25 03:07:05 +0100

    tmux config

--- config.sh
+++ config.sh
@@ -6,7 +6,6 @@ mkdir -p $HOME/.config
 # Create the symbolic links with the configurations
 ln -sf $PWD/starship/starship.toml $HOME/.config/starship.toml
 ln -sf $PWD/nvim $HOME/.config/nvim 
-ln -sf $PWD/tmux $HOME/.config/tmux
 ln -sf $PWD/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/nushell $HOME/.config/nushell
 ln -sf $PWD/bashrc $HOME/.bashrc  
