c3b7abf6a18c94cf180ed19c5e23ebb67384c72d
Voivodic <rodrigo.voivodic@gmail.com>
2025-01-25 05:42:00 +0100

    Line removing all previous configs

--- config.sh
+++ config.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Remove all current configurations
+rm -rf $HOME/.config $HOME/.config/starship.toml $HOME/.config/nvim $HOME/.tmux.conf $HOME/.config/nushell $HOME/.bashrc $HOME/.profile $HOME/.config/nix
+
 # Create the configuration file if not present
 mkdir -p $HOME/.config
 
 # Create the symbolic links with the configurations
-ln -sf $PWD/starship/starship.toml $HOME/.config/starship.toml
-ln -sf $PWD/nvim $HOME/.config/nvim 
-ln -sf $PWD/tmux/tmux.conf $HOME/.tmux.conf
-ln -sf $PWD/nushell $HOME/.config/nushell
-ln -sf $PWD/bashrc $HOME/.bashrc  
-ln -sf $PWD/profile $HOME/.profile
-ln -sf $PWD/nix $HOME/.config/nix
+ln -s $PWD/starship/starship.toml $HOME/.config/starship.toml
+ln -s $PWD/nvim $HOME/.config/nvim 
+ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/nushell $HOME/.config/nushell
+ln -s $PWD/bashrc $HOME/.bashrc  
+ln -s $PWD/profile $HOME/.profile
+ln -s $PWD/nix $HOME/.config/nix
 
 # Download tmp to add plugins to tmux
 git clone https://github.com/tmux-plugins/tpm.git $HOME/.tmux/plugins/tpm
