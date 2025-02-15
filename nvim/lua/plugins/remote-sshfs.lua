8fa4984055f174258586e4b27eaae50c9dd30b16
Voivodic <rodrigo.voivodic@gmail.com>
2025-02-15 01:52:47 +0100

    Keymaps changed to work on remote or local files

--- nvim/lua/plugins/remote-sshfs.lua
+++ nvim/lua/plugins/remote-sshfs.lua
@@ -6,9 +6,34 @@ return {
     },
     config = function()
         require("remote-sshfs").setup({})
-        vim.keymap.set("n", "<leader>rc", ":RemoteSSHFSConnect ")
-        vim.keymap.set('n', '<leader>rd', ":RemoteSSHFSDisconnect<CR>")
+
+        -- Set some keymaps for remote connection
+        local api = require('remote-sshfs.api')
+        vim.keymap.set("n", "<leader>rc", api.connect, {})
+        vim.keymap.set('n', '<leader>rd', api.disconnect, {})
+        vim.keymap.set('n', '<leader>rf', api.edit, {})
         
-        require("telescope").load_extension "remote-sshfs"
+        -- Tale the telescope and information about the remote connection
+        local builtin = require("telescope.builtin")
+        local connections = require("remote-sshfs.connections")
+
+        -- Define the find files for remote or local files
+        vim.keymap.set("n", "<leader>ff", function()
+            if connections.is_connected then
+                api.find_files()
+            else
+                builtin.find_files()
+            end
+        end, {})
+
+        -- Define the live grep for remote or local files
+        vim.keymap.set("n", "<leader>fg", function()
+            if connections.is_connected then
+                api.live_grep()
+            else
+                builtin.live_grep()
+            end
+        end, {})
+
     end,
 }
