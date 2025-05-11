02f23ed9656afd5d1cb755db5e493e6d826bbd34
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-05-11 15:28:28 -0300

    Addition of supermaven for code suggestions

---
+++ nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,21 @@
+return {
+    "supermaven-inc/supermaven-nvim",
+    name = "supermaven",
+    config = function()
+        require("supermaven-nvim").setup({  
+            keymaps = {
+                accept_suggestion = "<C-j>",
+                clear_suggestion = "<C-h>",
+                accept_word = "<C-l>",
+            },
+            ignore_filetypes = { }, -- or { "cpp", }
+            color = {
+                suggestion_color = "#f5e0dc",
+                cterm = 244,
+            },
+            log_level = "info", -- set to "off" to disable logging completely
+            disable_inline_completion = false, -- disables inline completion for use with cmp
+            disable_keymaps = false, -- disables built in keymaps for more manual control
+        })
+    end,
+}
