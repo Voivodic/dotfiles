430c49fb96615606f2e6b92cd375d336d4ee317c
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-05-12 19:38:22 -0300

    New plygin for working with git

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        name = "gitsigns",
+        config = function()
+            require("gitsigns").setup()
+
+            vim.keymap.set("n", "<leader>gv", ":Gitsigns preview_hunk<CR>", {})
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+        name = "fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", {})
+            vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
+            vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {})
+            vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {})
+        end,
+    }
+}
