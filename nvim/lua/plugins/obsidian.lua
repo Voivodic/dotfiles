0cc21417480addc27be0b96aac05a8b3d64bebf2
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-05-12 21:33:12 -0300

    Addition of obsidian plugin with new configs

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,39 +1,50 @@
 return {
-    "epwalsh/obsidian.nvim",
-    name = "obsidian",
-    version = "*",  -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-        "nvim-telescope/telescope.nvim",
-        "nvim-treesitter/nvim-treesitter",
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        name = 'render-markdown',
+        after = { 'nvim-treesitter' },
+        requires = { 'echasnovski/mini.nvim', opt = true }, 
+        config = function()
+            require('render-markdown').setup({})
+        end,
     },
-    config = function()
-        require("obsidian").setup({
-            ui = {
-                enable = false,
-            },
-            completion = {
-                nvim_cmp = true,
-                min_chars = 2,
-            },
-            notes_subdir = "notes",
-            new_notes_location = "notes_subdir",
-            workspaces = {
-                {
-                    name = "default",
-                    path = "~/GitRepos/obsidian",
+    {
+        "epwalsh/obsidian.nvim",
+        name = "obsidian",
+        version = "*",  -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+            "nvim-telescope/telescope.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("obsidian").setup({
+                ui = {
+                    enable = false,
                 },
-            },
-            templates = {
-                folder = "~/GitRepos/obsidian/templates",
-                date_format = "%d-%m-%Y",
-                time_format = "%H:%M",
-            },
-        })
+                completion = {
+                    nvim_cmp = true,
+                    min_chars = 2,
+                },
+                notes_subdir = "notes",
+                new_notes_location = "notes_subdir",
+                workspaces = {
+                    {
+                        name = "default",
+                        path = "~/GitRepos/obsidian",
+                    },
+                },
+                templates = {
+                    folder = "~/GitRepos/obsidian/templates",
+                    date_format = "%d-%m-%Y",
+                    time_format = "%H:%M",
+                },
+            })
 
-        vim.keymap.set("n", "<leader>ot", ":ObsidianTemplate note<CR>")
-    end,
+            vim.keymap.set("n", "<leader>ot", ":ObsidianTemplate note<CR>")
+        end,
+    },
 }
