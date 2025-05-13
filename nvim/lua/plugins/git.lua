f347325a8024d1217c5180d83195f42b0d4a9776
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-05-12 21:06:41 -0300

    Change in the commit command

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -13,7 +13,7 @@ return {
         name = "fugitive",
         config = function()
             vim.keymap.set("n", "<leader>ga", ":Git add", {})
-            vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
+            vim.keymap.set("n", "<leader>gc", ":Git commit -m", {})
             vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {})
             vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {})
         end,
