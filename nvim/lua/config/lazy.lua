4b0904e6d329dd7f26c424137ed3c175b26e9917
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-05-17 17:02:14 -0300

    Removed the hover plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "plugins" },
     -- Disable some of the plugins not used now
     { "yetone/avante.nvim", enabled = true},
+    { "lewis6991/hover.nvim", enabled = false},
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
