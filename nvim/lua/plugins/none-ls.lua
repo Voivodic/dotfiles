796304087401a1c4ea1ce8a8c17bc16bb683a7cc
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-06-09 00:49:17 -0300

    Removed python linter and formatting to replace by ruff

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,13 +9,11 @@ return {
         null_ls.setup({
             sources = {
                 -- Formatting tools
-                null_ls.builtins.formatting.black, -- python
                 null_ls.builtins.formatting.clang_format.with({
                     extra_args = { "-style=file" },
                 }), -- C
 
                 -- Linters
-                null_ls.builtins.diagnostics.pylint, -- python
                 null_ls.builtins.diagnostics.cppcheck, -- C/C++
                 null_ls.builtins.diagnostics.rstcheck, -- rst
                 -- null_ls.builtins.diagnostics.codespell, -- spelling
