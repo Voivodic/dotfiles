709ffcc7d425d25a7f38bb14d630ca3531cc6d5d
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-10-25 19:00:03 -0300

    Setup script updated to remove older configurations before symlinking, check if it is running on nixos, and start hyprland for personal configuration

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ while [[ "$#" -gt 0 ]]; do
                 ENV_TYPE="$2"
                 shift
             else
-                echo "Error: --env requires an argument (personal or vps)."
+                echo -e "Error: --env requires an argument (personal or vps).\n"
                 exit 1
             fi
             ;;
         *)
-            echo "Unknown parameter passed: $1"
+            echo -e "Unknown parameter passed: $1\n"
             exit 1
             ;;
     esac
@@ -22,100 +22,123 @@ done
 
 # Check if curl is installed
 if ! command -v curl >/dev/null 2>&1; then
-     echo "curl is not installed! Please install curl to proceed.\n"
+     echo -e "curl is not installed! Please install curl to proceed.\n"
      exit 1
 fi
 
-# Check if nix is already running
-if [ -z "$NIX_GCROOT" ]; then
-    # Install nix (multi-user)
-    echo -e "Installing nix...\n"
-    {
-        sh <(curl -L https://nixos.org/nix/install) --no-daemon
-    } && echo -e "Nix installed!\n" || {
-        echo -e "An error ocurred while trying to install nix!\n"
-        exit 1
-    }
+# Check if the system is NixOS
+if grep -q "ID=nixos" /etc/os-release 2>/dev/null; then
+    echo -e "Running on NixOS. Skipping Nix installation and sourcing.\n"
+else
+    # Check if nix is already installed and sourced
+    if [ -z "$NIX_GCROOT" ]; then
+        # Install nix (multi-user)
+        echo -e "Installing nix...\n"
+        {
+            sh <(curl -L https://nixos.org/nix/install) --no-daemon
+        } && echo -e "Nix installed!\n" || {
+            echo -e "An error occurred while trying to install nix!\n"
+            exit 1
+        }
 
-    # Source the nix profile script
-    echo -e "Sourcing nix profile...\n"
-    {
-        . $HOME/.nix-profile/etc/profile.d/nix.sh 
-    } && echo -e "Nix sourced!\n" || {
-        echo -e "An error ocurred while trying to source nix!\n"
-        exit 1
-    }
+        # Source the nix profile script
+        echo -e "Sourcing nix profile...\n"
+        {
+            . $HOME/.nix-profile/etc/profile.d/nix.sh 
+        } && echo -e "Nix sourced!\n" || {
+            echo -e "An error occurred while trying to source nix!\n"
+            exit 1
+        }
+    else
+        echo -e "Nix is already installed and sourced. Skipping installation and sourcing.\n"
+    fi
 fi
 
 # Setup for personal machine
 if [ "$ENV_TYPE" = "personal" ]; then
-    echo "Running personal setup..."
+    echo -e "Running personal setup...\n"
 
     # Create the configuration folders (if not present)
-    echo "Creating configuration folders..."
+    echo -e "Creating configuration folders...\n"
     mkdir -p $HOME/.config
     mkdir -p $HOME/.ssh
 
+    # Remove the existing directory with configurations
+    echo -e "Removing existing configurations...\n"
+    for dir in nushell nvim nix ghostty hypr swaync themes wal waybar waypaper wlogout wofi zed; do
+        rm -rf $HOME/.config/$dir
+    done
+
     # Create the symbolic links with the configurations
-    echo "Creating symbolic links..."
-    ln -sf $PWD/starship/starship.toml $HOME/.config/starship.toml
-    ln -sf $PWD/nvim $HOME/.config/nvim 
+    echo -e "Creating symbolic links...\n"
+    ln -sf $PWD/starship/starship.toml $HOME/.config
     ln -sf $PWD/tmux/tmux.conf $HOME/.tmux.conf
-    ln -sf $PWD/nushell $HOME/.config/nushell
     ln -sf $PWD/bashrc $HOME/.bashrc  
     ln -sf $PWD/profile $HOME/.profile
-    ln -sf $PWD/nix $HOME/.config/nix
-    ln -sf $PWD/ghostty $HOME/.config/ghostty
-    ln -sf $PWD/hyprland/hypr $HOME/.config/hypr
-    ln -sf $PWD/hyprland/swaync $HOME/.config/swaync
-    ln -sf $PWD/themes $HOME/.config/themes
-    ln -sf $PWD/hyprland/wal $HOME/.config/wal
-    ln -sf $PWD/hyprland/waybar $HOME/.config/waybar
-    ln -sf $PWD/hyprland/waypaper $HOME/.config/waypaper
-    ln -sf $PWD/hyprland/wlogout $HOME/.config/wlogout
-    ln -sf $PWD/hyprland/wofi $HOME/.config/wofi
-    ln -sf $PWD/zed $HOME/.config/zed
-    ln -sf $PWD/ssh/config $HOME/.ssh/config
+    ln -sf $PWD/ssh/config $HOME/.ssh
+    ln -s $PWD/nushell $HOME/.config
+    ln -s $PWD/nvim $HOME/.config 
+    ln -s $PWD/nix $HOME/.config
+    ln -s $PWD/ghostty $HOME/.config
+    ln -s $PWD/hyprland/hypr $HOME/.config
+    ln -s $PWD/hyprland/swaync $HOME/.config
+    ln -s $PWD/themes $HOME/.config
+    ln -s $PWD/hyprland/wal $HOME/.config
+    ln -s $PWD/hyprland/waybar $HOME/.config
+    ln -s $PWD/hyprland/waypaper $HOME/.config
+    ln -s $PWD/hyprland/wlogout $HOME/.config
+    ln -s $PWD/hyprland/wofi $HOME/.config
+    ln -s $PWD/zed $HOME/.config
 
     # Run nixos-rebuild
-    echo "Downloading all packages and configuring the system..."
+    echo -e "Downloading all packages and configuring the system...\n"
     sudo nixos-rebuild switch --flake $HOME/.config/nix --upgrade
+
+    # Sourcing hyprland
+    echo -e "Sourcing hyprland...\n"
+    hyprctl reload
 
     # Manage tpm (Tmux Plugin Manager)
     TPM_DIR="$HOME/.tmux/plugins/tpm"
     if [ ! -d "$TPM_DIR" ]; then
-        echo "Cloning tpm (Tmux Plugin Manager)..."
+        echo -e "Cloning tpm (Tmux Plugin Manager)...\n"
         git clone https://github.com/tmux-plugins/tpm.git "$TPM_DIR"
     else
-        echo "tpm (Tmux Plugin Manager) already exists. Updating..."
+        echo -e "tpm (Tmux Plugin Manager) already exists. Updating...\n"
         (cd "$TPM_DIR" && git pull)
     fi
 
     # Install/update tmux plugins
-    echo "Installing/updating tmux plugins..."
+    echo -e "Installing/updating tmux plugins...\n"
     . "$TPM_DIR/scripts/install_plugins.sh"
 
-    echo "Setup complete!"
+    echo -e "Setup complete!\n"
 
 # Setup for vps
 elif [ "$ENV_TYPE" = "vps" ]; then
-    echo "Running vps setup..."
+    echo -e "Running vps setup...\n"
 
     # Create the configuration foldes (if not present)
-    echo "Creating configuration folders..."
+    echo -e "Creating configuration folders...\n"
     mkdir -p $HOME/.config
     mkdir -p $HOME/.ssh
 
+    # Remove the existing directory with configurations
+    echo -e "Removing existing configurations...\n"
+    for dir in nushell nvim nix; do
+        rm -rf $HOME/.config/$dir
+    done
+
     # Create the symbolic links with the configurations
-    echo "Creating symbolic links..."
-    ln -sf $PWD/starship/starship.toml $HOME/.config/starship.toml
-    ln -sf $PWD/nvim $HOME/.config/nvim 
+    echo -e "Creating symbolic links...\n"
+    ln -sf $PWD/starship/starship.toml $HOME/.config
     ln -sf $PWD/tmux/tmux.conf $HOME/.tmux.conf
-    ln -sf $PWD/nushell $HOME/.config/nushell
     ln -sf $PWD/bashrc $HOME/.bashrc  
     ln -sf $PWD/profile $HOME/.profile
-    ln -sf $PWD/nix $HOME/.config/nix
-    ln -sf $PWD/ssh/config $HOME/.ssh/config
+    ln -sf $PWD/ssh/config $HOME/.ssh
+    ln -s $PWD/nushell $HOME/.config
+    ln -s $PWD/nvim $HOME/.config
+    ln -s $PWD/nix $HOME/.config
 
     # Install home-manager CLI
     if ! command -v home-manager >/dev/null 2>&1; then
@@ -129,26 +152,26 @@ elif [ "$ENV_TYPE" = "vps" ]; then
     fi
 
     # Run home-manager
-    echo "Downloading all packages and configuring the user ..."
+    echo -e "Downloading all packages and configuring the user ...\n"
     home-manager switch --flake $HOME/.config/nix --upgrade
 
     # Manage tpm (Tmux Plugin Manager)
     TPM_DIR="$HOME/.tmux/plugins/tpm"
     if [ ! -d "$TPM_DIR" ]; then
-        echo "Cloning tpm (Tmux Plugin Manager)..."
+        echo -e "Cloning tpm (Tmux Plugin Manager)...\n"
         git clone https://github.com/tmux-plugins/tpm.git "$TPM_DIR"
     else
-        echo "tpm (Tmux Plugin Manager) already exists. Updating..."
+        echo -e "tpm (Tmux Plugin Manager) already exists. Updating...\n"
         (cd "$TPM_DIR" && git pull)
     fi
 
     # Install/update tmux plugins
-    echo "Installing/updating tmux plugins..."
+    echo -e "Installing/updating tmux plugins...\n"
     . "$TPM_DIR/scripts/install_plugins.sh"
 
-    echo "Setup complete!"
+    echo -e "Setup complete!\n"
 
 else
-    echo "Unknown environment. Skipping setup."
+    echo -e "Unknown environment. Skipping setup.\n"
     exit 1
 fi
