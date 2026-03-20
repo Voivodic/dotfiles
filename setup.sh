70a387fe12c2196aaa82ee4f31bbb38d520b412c
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2026-03-20 17:27:43 -0500

    Changing the setup of vps to directly use nix instead of home-manager.

--- setup.sh
+++ setup.sh
@@ -146,20 +146,9 @@ elif [ "$ENV_TYPE" = "vps" ]; then
     ln -s $PWD/nix-general/hosts/vps $HOME/.config/nix/hosts
     ln -sf $PWD/nix-general/hosts/common-home.nix $HOME/.config/nix/hosts/common-home.nix
 
-    # Install home-manager CLI
-    if ! command -v home-manager >/dev/null 2>&1; then
-        echo -e "Installing home-manager CLI...\n"
-        {
-            nix profile install nixpkgs#home-manager
-        } && echo -e "Home Manager CLI installed!\n" || {
-            echo -e "An error occurred while trying to install Home Manager CLI!\n"
-            exit 1
-        }
-    fi
-
-    # Run home-manager
-    echo -e "Downloading all packages and configuring the user ...\n"
-    home-manager switch --flake $HOME/.config/nix#vps
+    # Run nixos-rebuild
+    echo -e "Downloading all packages and configuring the system...\n"
+    sudo nixos-rebuild switch --flake $HOME/.config/nix#vps
 
     # Manage tpm (Tmux Plugin Manager)
     TPM_DIR="$HOME/.tmux/plugins/tpm"
