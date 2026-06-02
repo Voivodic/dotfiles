6c02c88c035108a2894425fe613ecedd5d926df5
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2026-06-02 11:01:22 -0500

    Description added to the git-nvim keymaps.

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -12,10 +12,11 @@ return {
         "tpope/vim-fugitive",
         name = "fugitive",
         config = function()
-            vim.keymap.set("n", "<leader>ga", ":Git add %<CR>", {})
-            vim.keymap.set("n", "<leader>gc", ":Git commit -m \"\"<Left>", {})
-            vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {})
-            vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {})
+            vim.keymap.set("n", "<leader>ga", ":Git add %<CR>", { desc = "Add current file" })
+            vim.keymap.set("n", "<leader>gc", ":Git commit -m \"\"<Left>", { desc = "Commit" })
+            vim.keymap.set("n", "<leader>gs", ":Git status<CR>", { desc = "Status" })
+            vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Push" })
+
         end,
     }
 }
