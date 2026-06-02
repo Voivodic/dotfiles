18f70719d3d129cd3a15aaaf36afb9fdc1a6c2db
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2026-06-02 11:02:00 -0500

    lsp-config.lua changed to the new (nvim 0.12) standard.

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -2,37 +2,29 @@ return {
     "neovim/nvim-lspconfig",
     name = "nvim-lspconfig",
     config = function()
+        -- 1. Setup autocompletion capabilities
         local capabilities =
-        require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-        -- Define and configure the LSP servers
-        vim.lsp.config("clangd", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("pylsp", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("ruff", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("zls", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("nixd", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("gopls", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("nushell", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("templ", {
-            capabilities = capabilities,
-        })
-        vim.lsp.config("texlab", {
-            capabilities = capabilities,
-        })
+        -- 2. Configure individual servers with capabilities
+        -- This overrides/appends your capabilities to nvim-lspconfig's defaults
+        local servers = {
+            "clangd",
+            "pylsp",
+            "ruff",
+            "zls",       -- Zig Language Server
+            "nixd",
+            "gopls",
+            "nushell",
+            "templ",
+            "texlab",
+        }
+
+        for _, server in ipairs(servers) do
+            vim.lsp.config(server, { capabilities = capabilities })
+        end
+
+        -- 3. Configure servers that need custom properties
         vim.lsp.config("ltex", {
             capabilities = capabilities,
             cmd = { "ltex-ls-plus" },
@@ -62,10 +54,18 @@ return {
             },
         })
 
-        vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {})
+        -- 4. Activate/Enable all configurations for autostart
+        -- (You can append "ltex" right into the loop array, or pass the full list)
+        table.insert(servers, "ltex")
+        vim.lsp.enable(servers)
 
+        -- Keymaps
+        vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Hover" })
+        vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "Definition" })
+        vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "Code Action" })
+        vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format" })
+
+        -- Diagnostics
         vim.diagnostic.config({
             virtual_text = {
                 prefix = "●",
