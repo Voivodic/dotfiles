b198277b4aeb7624c6eb111bc3d93b100958a764
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2026-06-04 18:23:51 -0500

    Git diff shortcut added to nvim.

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -13,6 +13,7 @@ return {
         name = "fugitive",
         config = function()
             vim.keymap.set("n", "<leader>ga", ":Git add %<CR>", { desc = "Add current file" })
+            vim.keymap.set("n", "<leader>gd", ":Git diff<CR>", { desc = "Diff" })
             vim.keymap.set("n", "<leader>gc", ":Git commit -m \"\"<Left>", { desc = "Commit" })
             vim.keymap.set("n", "<leader>gs", ":Git status<CR>", { desc = "Status" })
             vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Push" })
